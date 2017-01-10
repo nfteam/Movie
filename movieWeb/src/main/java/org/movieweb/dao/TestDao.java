4094b1f06625447806188183fb2d66c5652cad7e
liang <1240557212@qq.com>
2017-01-10 19:45:10 +0800

    add domian

--- movieWeb/src/main/java/org/movieweb/dao/TestDao.java
+++ movieWeb/src/main/java/org/movieweb/dao/TestDao.java
@@ -1,12 +1,16 @@
 package org.movieweb.dao;
 
-import org.domian.utils.HibernateUtil;
-
 /**
  * Created by LIANG on 2017/1/10.
  */
 public class TestDao {
-    public static void main(String[] args) {
-        System.out.println( HibernateUtil.getEntityManager());
+//    public static void main(String[] args) {
+//        System.out.println( HibernateUtil.getEntityManager());
+//    }
+
+    public  String mytest(){
+        System.out.println("---------------");
+        String message="测试";
+        return "success";
     }
 }
