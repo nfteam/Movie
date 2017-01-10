e5fb5265bb6fde6d28a8254ad29b2dd56925b95e
liang <1240557212@qq.com>
2017-01-10 20:24:13 +0800

    add domian

--- movieWeb/src/main/java/org/movieweb/dao/TestDao.java
+++ movieWeb/src/main/java/org/movieweb/dao/TestDao.java
@@ -4,13 +4,22 @@ package org.movieweb.dao;
  * Created by LIANG on 2017/1/10.
  */
 public class TestDao {
-//    public static void main(String[] args) {
+    //    public static void main(String[] args) {
 //        System.out.println( HibernateUtil.getEntityManager());
 //    }
+    private String msg;
 
-    public  String mytest(){
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String mytest() {
         System.out.println("---------------");
-        String message="测试";
+        msg = "测试";
         return "success";
     }
 }
