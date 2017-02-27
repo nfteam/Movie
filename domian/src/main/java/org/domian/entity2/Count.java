773f64bb721056a5709a5a83f19199d7c288dc80
liang <1240557212@qq.com>
2017-02-27 15:41:50 +0800

    add domian

---
+++ domian/src/main/java/org/domian/entity2/Count.java
@@ -0,0 +1,56 @@
+package org.domian.entity2;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * Created by LIANG on 2017/2/24.
+ */
+@Entity
+@Table(name = "count_info")
+public class Count {
+    private String cId;
+    private String typeNname;
+    private int count;
+    private Date time;
+
+    @Id
+    @Column(name = "c_id")
+    public String getcId() {
+        return cId;
+    }
+
+    public void setcId(String cId) {
+        this.cId = cId;
+    }
+
+    @Column(name = "typeName")
+    public String getTypeNname() {
+        return typeNname;
+    }
+
+    public void setTypeNname(String typeNname) {
+        this.typeNname = typeNname;
+    }
+
+    @Column(name = "time")
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    @Column(name = "count")
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
